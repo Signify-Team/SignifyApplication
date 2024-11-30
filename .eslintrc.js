48d52847d36f04dccc1937bfea7071cf41819de8
Dila Tosun <dilatosun@gmail.com>
2024-11-30 20:17:30 +0300

    Revert "eslint file updates"
    This reverts commit b19414d8b24e1ddaefa0461d7aac4064204dcde3.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-    'google',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'react-native',
-  ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': 'off', 
-  },
+  root: true,
+  extends: '@react-native',
 };

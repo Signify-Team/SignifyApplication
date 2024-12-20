b9ff342755f791798356a876ebd1e310b8fdaadb
Dila Tosun <dilatosun@gmail.com>
2024-12-20 14:34:39 +0300

    Update AnswerCard.js

--- front-end-layer/components/AnswerCard.js
+++ front-end-layer/components/AnswerCard.js
@@ -14,9 +14,8 @@ import styles from '../styles/AnswerCardStyles';
 const AnswerCard = ({ answer, isSelected, isCorrect, onPress, isAnswered }) => {
     const getBackgroundColor = () => {
         if (!isAnswered) {return COLORS.soft_pink_background;}
-        if (isSelected && isCorrect) {return COLORS.tertiary;} // Green for correct
-        if (isSelected && !isCorrect) {return COLORS.highlight_color_2;} // Red for incorrect
-        if (!isSelected && isCorrect) {return COLORS.tertiary;} // Green for the correct answer if selected wrong
+        if (isSelected) { return isCorrect ? COLORS.tertiary : COLORS.highlight_color_2; } // Green if correct, Red if incorrect 
+        if (isCorrect) { return COLORS.tertiary; }// Green for the correct answer even if not selected
         return COLORS.soft_pink_background;
     };
 

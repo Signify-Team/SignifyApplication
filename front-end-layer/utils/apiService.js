985dfb6472d9b8a70ae1546c78c1461ca429d2b6
Zeynep Su Ucan <zeynep.ucan@commencis.com>
2025-03-31 02:29:49 +0300

    Separate and Create Services

--- front-end-layer/utils/apiService.js
+++ front-end-layer/utils/apiService.js
@@ -11,6 +11,7 @@ export * from './services/languageService';
 export * from './services/questService';
 export * from './services/courseService';
 export * from './services/badgeService';
+
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 

82d8db75becf476d6617f8fb4ce1431f8a2fb510
Zeynep Su Ucan <zeynep.ucan@commencis.com>
2025-04-21 17:44:06 +0300

    Add Course Progress

--- front-end-layer/utils/config.js
+++ front-end-layer/utils/config.js
@@ -5,5 +5,5 @@
  * @lastmodified 31.03.2025
  */
 
-export const API_BASE_URL = 'http://192.168.31.123:3000/api';
-export const USER_ID_KEY = '@user_id';
+export const API_BASE_URL = process.env.API_URL; // here you might need to give the public ip hardcoded will fix this bug later on
+export const USER_ID_KEY = '@user_id'; 

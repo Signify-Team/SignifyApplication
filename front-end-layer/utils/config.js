3e79eea8a9cc9d0eb2c6cbb48944c593d369976b
Dila Tosun <dilatosun@gmail.com>
2025-04-24 19:20:07 +0300

    ip change

--- front-end-layer/utils/config.js
+++ front-end-layer/utils/config.js
@@ -5,5 +5,5 @@
  * @lastmodified 31.03.2025
  */
 
-export const API_BASE_URL = 'http://192.168.31.123:3000/api';
+export const API_BASE_URL = 'http://192.168.1.116:3000/api';
 export const USER_ID_KEY = '@user_id';
